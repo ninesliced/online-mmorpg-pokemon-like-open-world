7bd93f685522d25d54d7a93aa72e4bf43ca7c3d1
ARKANYOTA <arkanyota@icloud.com>
2026-06-06 17:57:24 +0200

    G

--- src/main.gd
+++ src/main.gd
@@ -1,15 +1,11 @@
 extends Node2D
 
-const DEFAULT_ADDRESS = "172.20.10.2"
 const PORT = 12345
 
-@onready var host_button: Button = $Control/JoinMenu/HostButton
-@onready var ip_input: TextEdit = $Control/JoinMenu/IPInput
-@onready var connect_button: Button = $Control/JoinMenu/ConnectButton
-@onready var chat: Chat = $Chat
-@onready var connected_player_label: Label = $Control/ConnectedPlayerLabel
+@onready var host_button: Button = $Control/HostButton
+@onready var ip_input: TextEdit = $Control/IPInput
+@onready var connect_button: Button = $Control/ConnectButton
 
-var players = {}
 
 func _ready() -> void:
 	multiplayer.peer_connected.connect(_player_connected)
@@ -18,21 +14,10 @@ func _ready() -> void:
 	multiplayer.connection_failed.connect(_connected_fail)
 	multiplayer.server_disconnected.connect(_server_disconnected)
 
-func _process(delta: float) -> void:
-	connected_player_label.text = "Connected: "
-	var players_sorted = players.keys()
-	players_sorted.sort()
-	for player in players_sorted:
-		connected_player_label.text += str(player) + ", "
-
-## Buttons
 
 func _on_connect_button_pressed() -> void:
-	var ip = DEFAULT_ADDRESS
-	if not ip_input.text.is_empty():
-		ip = ip_input.text
-	print("Connecting to %s:%s" % [ip, str(PORT)])
-	_create_client(ip, PORT)
+	print("Connecting to %s:%s" % [ip_input.text, str(PORT)])
+	_create_client(ip_input.text, PORT)
 
 
 func _on_host_button_pressed() -> void:
@@ -47,7 +32,7 @@ func _create_client(server_ip: String, server_port: int):
 	if err:
 		print("Error client: ", err)
 	print("Id: ", multiplayer.get_unique_id())
-	chat.add_message("Connected to %s:%s" % [server_ip, server_port])
+	
 
 
 func _create_server(port: int):
@@ -57,30 +42,22 @@ func _create_server(port: int):
 	if err:
 		print("Error server: ", err)
 	print("Id: ", multiplayer.get_unique_id())
-	chat.add_message("Server created at port %s" % [port])
-	
 
 
 ## Signaux
-
 func _player_connected(id: int):
 	print("_player_connected %s" % [id])
-	chat.add_message("Player connected: id = %s" % [id])
-	players[id] = true
+	if is_multiplayer_authority():
+		$Game/MultiplayerSpawner.add_spawnable_scene("res://src/player.tscn")
 
 func _player_disconnected(id: int):
 	print("_player_disconnected %s" % [id])
-	chat.add_message("Player disconnected: id = %s" % [id])
-	players[id] = false
 
 func _connected_ok():
-	chat.add_message("Successfully connected to server")
 	print("_connected_ok")
 
 func _connected_fail():
-	chat.add_message("Failed connection to server")
 	print("_connected_fail")
 
 func _server_disconnected():
-	chat.add_message("Server disconnected")
 	print("_server_disconnected")
